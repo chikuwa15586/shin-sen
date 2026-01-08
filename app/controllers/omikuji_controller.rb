f8b7eec59d5bf29224e877cb282a865c91eafc02
mgamo <m.gamo012@outlook.jp>
2026-01-08 22:56:43 +0900

    ログイン機能追加

--- app/controllers/omikuji_controller.rb
+++ app/controllers/omikuji_controller.rb
@@ -1,5 +1,15 @@
 class OmikujiController < ApplicationController
   def show
-    @fortune = Fortune.order("RANDOM()").first
+    today = Date.current.to_s
+
+    if session[:last_draw_date] == today
+      # すでに引いている場合
+      @fortune = Fortune.find(session[:fortune_id])
+    else
+      # まだ引いていない場合
+      @fortune = Fortune.order("RANDOM()").first
+      session[:last_draw_date] = today
+      session[:fortune_id] = @fortune.id
+    end
   end
 end
